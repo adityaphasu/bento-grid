05214ffec63e2b661407d9f8be43a31f8eea035b
Aditya Phasu <adityaphasu919@gmail.com>
2024-12-06 21:54:30 +0530

    Add initial files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "purple-primary": "hsl(254, 88%, 90%)",
+        "purple-secondary": "hsl(256, 67%, 59%)",
+        "yellow-primary": "hsl(31, 66%, 93%)",
+        "yellow-secondary": "hsl(39, 100%, 71%)",
+        "blackish": "hsl(0, 0%, 7%)",
+      }
+    },
+  },
+  plugins: [],
+}
+
