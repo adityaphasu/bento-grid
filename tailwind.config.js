5611639002ae31b16cfabbe2aa2ddfca8cfb1ff9
Aditya Phasu <adityaphasu919@gmail.com>
2025-01-04 03:51:32 +0530

    refactor card styles and grid layout for improved responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,17 @@ export default {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        xl: "1440px",
+      },
       colors: {
         "purple-primary": "hsl(254, 88%, 90%)",
         "purple-secondary": "hsl(256, 67%, 59%)",
         "yellow-primary": "hsl(31, 66%, 93%)",
         "yellow-secondary": "hsl(39, 100%, 71%)",
-        "blackish": "hsl(0, 0%, 7%)",
-      }
+        blackish: "hsl(0, 0%, 7%)",
+      },
     },
   },
   plugins: [],
-}
-
+};
